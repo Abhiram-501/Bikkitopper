fd6a8075216702e5925ebb3de75d3db3cd4eb5bc
Abhiram-501 <abhinaidu0705@gmail.com>
2024-09-18 10:09:33 +0530

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<string.h>
+char str[10],out,in,output[10],input[10],temp;
+char tl[10]={'x','+','*','(',')','$','@'};
+char nt[10]={'E','e','T','t',F};
+int err=0,flag=0;i,j,k,l,m;
+char c[10][10][7]={{{"Te"},{"ERROR!"},{"Te"},{"ERROR!"},{"ERROR!"}},
+{"ERROR!","+Te",}}
